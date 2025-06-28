40691d65dd7e72e5ecd193de92a3b1317b169018
drwwwww <ledanreba@gmail.com>
2025-06-28 03:22:39 -0400

    Render work: adding "flask_cors" to rquirements

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+notes = []
+
+@app.route("/addNote", methods=["POST"])
+def addNote():
+    data = request.get_json()
+
+    title = data.get("title")
+    content = data.get("content")
+
+    note = {"title": title, "content": content}
+
+    notes.append(note)
+
+    return
