91c884e8167718dd8ad9ed9efa2d2a508112e12c
drwwwww <ledanreba@gmail.com>
2025-06-28 03:46:18 -0400

    Testing to see database compatibility

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import sqlite3
+
+def initData():
+    base = sqlite3.connect("database.db")
+    c = base.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+    """)
+    c.commit()
+    c.close()
 
 app = Flask(__name__)
+initData()
 CORS(app)
-
-notes = []
 
 @app.route("/addNote", methods=["POST"])
 def addNote():
@@ -13,8 +26,10 @@ def addNote():
     title = data.get("title")
     content = data.get("content")
 
-    note = {"title": title, "content": content}
-
-    notes.append(note)
-
+    base = sqlite3.connect("database.db")
+    c = base.cursor()
+    c.execute("INSERT INTO notes (title, content) VALUES (?, ?)" title, content)
+    c.commit()
+    c.close()
+    
     return jsonify({"message": "done"})
